23e6c5dd1c48d43c6d40c99367c79578f1f14640
maggiolil <luca.maggioli@nagra.com>
2022-01-05 10:27:14 +0100

    add: migration and database update

--- facturator-api-dotnetcore/facturator-api/Migrations/20220105091744_IsArchived_in_Article.Designer.cs
+++ facturator-api-dotnetcore/facturator-api/Migrations/20220105091744_IsArchived_in_Article.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using facturator_api.Models.Context;
 
 namespace facturator_api.Migrations
 {
     [DbContext(typeof(FacturatorDbContext))]
-    partial class FacturatorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220105091744_IsArchived_in_Article")]
+    partial class IsArchived_in_Article
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ facturator-api-dotnetcore/facturator-api/Migrations/20220105091744_IsArchived_in_Article.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace facturator_api.Migrations
+{
+    public partial class IsArchived_in_Article : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Articles",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Articles");
+        }
+    }
+}
