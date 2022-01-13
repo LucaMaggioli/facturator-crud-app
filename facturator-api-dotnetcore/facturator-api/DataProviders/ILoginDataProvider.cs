cafdafc310bb20514034c299a8871069d7a140be
maggiolil <luca.maggioli@nagra.com>
2022-01-13 09:03:26 +0100

    add: interfaces and todo file

---
+++ facturator-api-dotnetcore/facturator-api/DataProviders/ILoginDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using facturator_api.Models;
+using System.Threading.Tasks;
+
+namespace facturator_api.DataProviders
+{
+    public interface ILoginDataProvider
+    {
+        Task<bool> UserNameExists(string username);
+        Task<Vendor> VendorLogin(string username, string password);
+        Task<Vendor> VendorSingin(string username, string password, Vendor newVendor);
+    }
+}
