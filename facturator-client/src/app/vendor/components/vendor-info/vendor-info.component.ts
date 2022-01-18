f4d99ba7fadfe5b8b3417e4bf0e1948f17e9e200
maggiolil <luca.maggioli@nagra.com>
2022-01-18 11:51:00 +0100

    fix: fixing vendor info form can now save only if form is modified

--- facturator-client/src/app/vendor/components/vendor-info/vendor-info.component.ts
+++ facturator-client/src/app/vendor/components/vendor-info/vendor-info.component.ts
@@ -10,6 +10,9 @@ import {VendorDto} from "../../dtos/VendorDto";
   styleUrls: ['./vendor-info.component.css']
 })
 export class VendorInfoComponent implements OnInit {
+
+  canSave:boolean = false;
+
   firstName = new FormControl('');
   lastName = new FormControl('');
   companyName = new FormControl('');
@@ -34,35 +37,19 @@ export class VendorInfoComponent implements OnInit {
     vendorData.firstName != null ? this.firstName.setValue(vendorData.firstName) : this.firstName.setValue('');
     vendorData.lastName != null ? this.lastName.setValue(vendorData.lastName) : this.lastName.setValue('');
     vendorData.companyName != null ? this.companyName.setValue(vendorData.companyName) : this.companyName.setValue('');
+    this.subrscribeToFormChanges();
+    this.canSave = false;
   }
 
   saveData(){
     console.log("entering saveData");
     this._vendorService.UpdateVendor({"firstName":this.firstName.value,"lastName":this.lastName.value,"companyName":this.companyName.value});
+    this.canSave = false;
+  }
+
+  subrscribeToFormChanges(){
+    this.firstName.valueChanges.subscribe((val)=>{this.canSave = true});
+    this.lastName.valueChanges.subscribe((val)=>{this.canSave = true});
+    this.companyName.valueChanges.subscribe((val)=>{this.canSave = true});
   }
 }
-
-
-
-/*
-* console.log("from vendor page");
-    let fromAuthService = this._authService.currentUser;
-    console.log(fromAuthService);
-
-    if(fromAuthService !== null){
-      console.log("user is not null so ")
-      this.firstName.setValue("firstname");
-      //this.firstName.setValue(fromAuthService.firstName);
-      this.lastName.setValue("xxxxx lastname");
-      //this.lastName.setValue(fromAuthService.lastName);
-      this.companyName.setValue("xxx company");
-      //this.companyName.setValue(fromAuthService.companyName);
-    }
-    /*if (loggedUser != null) {
-      this.currentUser = new VendorDto(loggedUser['firstName'], loggedUser['lastName'], loggedUser['companyName'])
-
-//    console.log(this._authService.currentUser["firstName"])
-this._authService.currentUser.firstName !== null ? this.firstName = this._authService.currentUser["firstName"] : "";
-this._authService.currentUser.lastName !== null ? this.lastName = this._authService.currentUser["lastName"] : "";
-this._authService.currentUser.companyName !== null ? this.companyName = this._authService.currentUser["companyName"] : "";
-* */
